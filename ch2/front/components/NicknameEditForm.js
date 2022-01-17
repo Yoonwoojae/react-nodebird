8c4b0c008c60cf312acf11e4b2d66215b8098f4b
yoonwoojae <dbsdnwp1004@naver.com>
2022-01-17 10:04:55 +0900

    modi

--- ch2/front/components/NicknameEditForm.js
+++ ch2/front/components/NicknameEditForm.js
@@ -1,9 +1,16 @@
 import { Form, Input } from 'antd';
-import React from 'react';
+import React, {useMemo} from 'react';
 
 const NicknameEditForm = () => {
+
+  const FormStyle = useMemo(() => ({
+    marginBottom: '20px',
+    border: '1px solid #d9d9d9',
+    padding: '20px'
+  }));
+
   return (
-    <Form style={{ marginBottom: '20px', border: '1px solid #d9d9d9', padding: '20px' }}>
+    <Form style={FormStyle}>
       <Input.Search addonBefore="닉네임" enterButton="수정" />
     </Form>
   );
